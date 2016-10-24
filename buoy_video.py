2a296bcfa71d32ab856754eb89793341d571963a
edinpeter <edin.3@osu.edu>
2016-10-24 13:38:44 -0400

    added parameters, removed prints

--- buoy_video.py
+++ buoy_video.py
@@ -10,11 +10,11 @@ upper_red = np.array([179,255,255])
 
 cap = cv2.VideoCapture('output.mp4')
 
-def findGreenBuoy(frame):
+def findColorBuoy(frame, lower, upper, color):
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
-	mask = cv2.inRange(hsv, lower_green, upper_green)
+	mask = cv2.inRange(hsv, lower, upper)
 
 	(_, cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 	if(len(cnts) > 0):
@@ -30,52 +30,12 @@ def findGreenBuoy(frame):
 			contours_y = contours_y + line[0][1]
 			# print(contours_x)
 		if(contours_count > 0):
-			print(contours_count)
-
 			contours_x = contours_x / contours_count
 			contours_y = contours_y / contours_count
 
 			center = (contours_x,contours_y)
 			cv2.circle(frame,center,10,(255,0,255),-1)
-			cv2.putText(frame,'GREEN',(center[0],center[1]), 1, 2,(255,255,255), 1,cv2.LINE_AA)
-		approx = cv2.approxPolyDP(c, 0.1 * peri, True)
-		cv2.drawContours(frame, [approx], -1, (0, 255, 0), 3)
-
-		x,y,w,h = cv2.boundingRect(c)
-		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,255),2)
-
-		(x,y),radius = cv2.minEnclosingCircle(c)
-		center = (int(x),int(y))
-		radius = int(radius)
-		cv2.circle(frame,center,radius,(255,0,255),2)
-def findRedBuoy(frame):
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # define range of blue color in HSV
-	mask = cv2.inRange(hsv, lower_red, upper_red)
-
-	(_, cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	if(len(cnts) > 0):
-		c = max(cnts, key=cv2.contourArea)
-		peri = cv2.arcLength(c, True)
-
-		contours_x = 0
-		contours_y = 0
-		contours_count = 0
-		for line in c:
-			contours_count = contours_count + 1
-			contours_x = contours_x + line[0][0]
-			contours_y = contours_y + line[0][1]
-			# print(contours_x)
-		if(contours_count > 0):
-			print(contours_count)
-
-			contours_x = contours_x / contours_count
-			contours_y = contours_y / contours_count
-
-			center = (contours_x,contours_y)
-			cv2.circle(frame,center,10,(255,0,255),-1)
-			cv2.putText(frame,'RED',(center[0],center[1]), 1, 2,(255,255,255), 1,cv2.LINE_AA)
+			cv2.putText(frame,color,center, 1, 2,(255,255,255), 1,cv2.LINE_AA)
 		approx = cv2.approxPolyDP(c, 0.1 * peri, True)
 		cv2.drawContours(frame, [approx], -1, (0, 255, 0), 3)
 
@@ -92,8 +52,8 @@ while(cap.isOpened()):
 	ret, frame = cap.read()
     
     #ret, mask = cv2.threshold(frame, 10, 255, cv2.THRESH_BINARY)
-	findGreenBuoy(frame)
-	findRedBuoy(frame)
+	findColorBuoy(frame, lower_green, upper_green, "Green")
+	findColorBuoy(frame, lower_red, upper_red, "Red")
 	cv2.imshow('frame',frame)
 
     #####################################
